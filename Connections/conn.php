ba43afe649b3bf953b03e15410b7df215a7d25b4
Charles <epson12332@gmail.com>
2016-04-08 09:41:21 +0800

    online_aws

--- Connections/conn.php
+++ Connections/conn.php
@@ -2,11 +2,11 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_conn = "127.0.0.1";
+$hostname_conn = "love.chgtvkwpqf60.ap-northeast-1.rds.amazonaws.com:3306";
 $database_conn = "love";
-$username_conn = "root";
-$password_conn = "5; u.4t; ";
-$conn = mysql_pconnect($hostname_conn, $username_conn, $password_conn) or trigger_error(mysql_error(),E_USER_ERROR); 
+$username_conn = "epson12332";
+$password_conn = "aabb1234";
+$conn = mysql_pconnect($hostname_conn, $username_conn, $password_conn) or trigger_error(mysql_error(),E_USER_ERROR);
 mysql_set_charset('utf8');
 //先設定時區
 date_default_timezone_set("Asia/Taipei");
