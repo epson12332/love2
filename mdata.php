b7d7b0cea514ed576993127103963e8137209a4a
Charles <epson12332@gmail.com>
2016-04-09 21:42:53 +0800

    個人資料

--- mdata.php
+++ mdata.php
@@ -1,39 +1,10 @@
 <?php require_once('Connections/conn.php'); ?>
 <?php
-//initialize the session
 if (!isset($_SESSION)) {
   session_start();
 }
-
-// ** Logout the current user. **
-$logoutAction = $_SERVER['PHP_SELF']."?doLogout=true";
-if ((isset($_SERVER['QUERY_STRING'])) && ($_SERVER['QUERY_STRING'] != "")){
-  $logoutAction .="&". htmlentities($_SERVER['QUERY_STRING']);
-}
-
-if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
-  //to fully log out a visitor we need to clear the session varialbles
-  $_SESSION['MM_Username'] = NULL;
-  $_SESSION['MM_UserGroup'] = NULL;
-  $_SESSION['PrevUrl'] = NULL;
-  $_SESSION['char']= $row_Recordset1['D_CHAR'];;
-  unset($_SESSION['MM_Username']);
-  unset($_SESSION['MM_UserGroup']);
-  unset($_SESSION['PrevUrl']);
-	
-  $logoutGoTo = "index.php";
-  if ($logoutGoTo) {
-    header("Location: $logoutGoTo");
-    exit;
-  }
-}
-?>
-<?php
-if (!isset($_SESSION)) {
-  session_start();
-}
-$MM_authorizedUsers = "小主人";
-$MM_donotCheckaccess = "false";
+$MM_authorizedUsers = "";
+$MM_donotCheckaccess = "true";
 
 // *** Restrict Access To Page: Grant or deny access to this page
 function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) { 
@@ -54,7 +25,7 @@ function isAuthorized($strUsers, $strGroups, $UserName, $UserGroup) {
     if (in_array($UserGroup, $arrGroups)) { 
       $isValid = true; 
     } 
-    if (($strUsers == "") && false) { 
+    if (($strUsers == "") && true) { 
       $isValid = true; 
     } 
   } 
@@ -105,6 +76,31 @@ function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDe
 }
 }
 
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_update"])) && ($_POST["MM_update"] == "form1")) {
+  $updateSQL = sprintf("UPDATE member SET D_PWD=%s, D_NAME=%s, D_TEL=%s, D_ADDRESS=%s, D_EMAIL=%s WHERE D_ID=%s",
+                       GetSQLValueString($_POST['PWD'], "text"),
+                       GetSQLValueString($_POST['name'], "text"),
+                       GetSQLValueString($_POST['tel'], "text"),
+                       GetSQLValueString($_POST['add'], "text"),
+                       GetSQLValueString($_POST['email'], "text"),
+                       GetSQLValueString($_POST['hiddenField'], "text"));
+
+  mysql_select_db($database_conn, $conn);
+  $Result1 = mysql_query($updateSQL, $conn) or die(mysql_error());
+
+  $updateGoTo = "su.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $updateGoTo .= (strpos($updateGoTo, '?')) ? "&" : "?";
+    $updateGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $updateGoTo));
+}
+
 $colname_Recordset1 = "-1";
 if (isset($_SESSION['MM_Username'])) {
   $colname_Recordset1 = $_SESSION['MM_Username'];
@@ -115,23 +111,19 @@ $Recordset1 = mysql_query($query_Recordset1, $conn) or die(mysql_error());
 $row_Recordset1 = mysql_fetch_assoc($Recordset1);
 $totalRows_Recordset1 = mysql_num_rows($Recordset1);
 ?>
-<?php  if ($row_Recordset1['D_CHAR']='S'){
-	$cn='小天使';}
-	else if ($row_Recordset1['D_CHAR']='D'){$cn='小主人';}
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>操作目錄</title>
+<title>會員資料</title>
 <style type="text/css">
 #apDiv1 {
 	position: absolute;
 	width: 120px;
 	height: 53px;
 	z-index: 1;
-	left: 374px;
-	top: 220px;
+	left: 613px;
+	top: 213px;
 }
 #apDiv2 {
 	position: absolute;
@@ -146,16 +138,16 @@ $totalRows_Recordset1 = mysql_num_rows($Recordset1);
 	width: 119px;
 	height: 54px;
 	z-index: 3;
-	left: 535px;
-	top: 219px;
+	left: 654px;
+	top: 214px;
 }
 #apDiv4 {
 	position: absolute;
 	width: 118px;
 	height: 52px;
 	z-index: 4;
-	left: 651px;
-	top: 217px;
+	left: 791px;
+	top: 212px;
 }
 #apDiv5 {
 	position: absolute;
@@ -170,8 +162,8 @@ $totalRows_Recordset1 = mysql_num_rows($Recordset1);
 	width: 121px;
 	height: 53px;
 	z-index: 6;
-	left: 701px;
-	top: 222px;
+	left: 929px;
+	top: 213px;
 }
 #apDiv7 {
 	position: absolute;
@@ -194,16 +186,16 @@ $totalRows_Recordset1 = mysql_num_rows($Recordset1);
 	width: 437px;
 	height: 157px;
 	z-index: 9;
-	left: 398px;
-	top: 387px;
+	left: 471px;
+	top: 388px;
 }
 #apDiv10 {
 	position: absolute;
 	width: 63px;
 	height: 75px;
 	z-index: 10;
-	left: 41px;
-	top: -97px;
+	left: 44px;
+	top: -93px;
 }
 </style>
 </head>
@@ -213,19 +205,31 @@ $totalRows_Recordset1 = mysql_num_rows($Recordset1);
 <div id="apDiv7"><img src="圖/網頁用logo.png" width="170" height="168" /></div>
 <div id="apDiv8"><img src="圖/網頁標題.png" width="480" height="160" /></div>
 <div id="apDiv1"><a href="s_manal.php"><img src="圖/網頁icon回首頁.png" width="120" height="54" /></a></div>
-<div id="apDiv3"><a href="mdata.php"><img src="圖/網頁icon綠個人資料.png" width="120" height="54" /></a></div>
-<div id="apDiv6"><img src="圖/網頁icon(about us).png" width="120" height="54" /></div>
 <div id="apDiv9">
-<div><p>hi,小主人
-<?php   
-echo $row_Recordset1['D_NAME']; ?></p>
-  <p><a href="new_stuff.php">我需要物資</a>  </p>
-  <p><a href="stuff_supplied.php">小天使提供的物資</a></p>
-<p><a href="stuffdata.php">我所需物資列表</a></p>
-<p><a href="<?php echo $logoutAction ?>">登出</a></p>
+  <div>
+
+<form id="form1" name="form1" method="POST" action="<?php echo $editFormAction; ?>">
+  <label for="PWD"></label>
+  <p>帳號：
+  <?php echo $row_Recordset1['D_ID']; ?></p>
+  <p>
+    <label for="name">姓名</label>
+    ：<?php echo $row_Recordset1['D_NAME']; ?></p>
+  <p>
+    <label for="tel">電話：</label>
+    <?php echo $row_Recordset1['D_TEL']; ?></p>
+  <p>
+    <label for="email">email:</label>
+    <?php echo $row_Recordset1['D_EMAIL']; ?></p>
+  <p>
+    <label for="add">地址：</label>
+    <?php echo $row_Recordset1['D_ADDRESS']; ?></p>
+  <p>&nbsp;</p>
+</form>
 </div>
-<div id="apDiv10"><img src="圖/歡迎光臨.png" width="350" height="69" /></div>
+<div id="apDiv10"><img src="圖/會員資料修改.png" width="350" height="69" /></div>
 </div>
+
 </body>
 </html>
 <?php
